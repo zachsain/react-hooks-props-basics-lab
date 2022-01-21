65e281f180d90e6f55f1bf2d560e5f0daef8f01d
Zachary Sain <zachsain@gmail.com>
2022-01-21 14:47:20 -0600

    missing one test

--- src/components/About.js
+++ src/components/About.js
@@ -1,22 +1,36 @@
 import React from "react";
+import Links from "./Links";
 
-function BioPassed ({bio}) {
- if (bio === "") {
+// function BioPassed ({bio}) {
+//  if (bio === "") {
+//    return null
+//  } else {
+//    return <p>{bio}</p>
+//  }
+// }
+
+function About(props) {
+console.log(props)
+
+  function bioPassed () {
+ if (props.bio === "") {
    return null
  } else {
-   return <p>{bio}</p>
+   return <p>{props.bio}</p>
  }
 }
 
-function About(props) {
-
+  console.log(props)
 
   return (
     <div id="about">
       <h2>About Me</h2>
-      <BioPassed />
+      {bioPassed()}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      
+      <Links
+            github = {props.github}
+            linkedin = {props.linkedin} /> 
     </div>
   );
 }
